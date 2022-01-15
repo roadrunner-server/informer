022756524b28facf6dc199924afc343d52eaa18e
Valery Piashchynski <piashchynski.valery@gmail.com>
2022-01-15 15:03:52 +0300

    initial informer commit
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

---
+++ plugin.go
@@ -0,0 +1,76 @@
+package informer
+
+import (
+	"context"
+
+	"github.com/roadrunner-server/api/v2/plugins/informer"
+	"github.com/roadrunner-server/api/v2/plugins/jobs"
+	endure "github.com/roadrunner-server/endure/pkg/container"
+	"github.com/roadrunner-server/sdk/v2/state/process"
+)
+
+const PluginName = "informer"
+
+type Plugin struct {
+	withJobs    map[string]informer.JobsStat
+	withWorkers map[string]informer.Informer
+}
+
+func (p *Plugin) Init() error {
+	p.withWorkers = make(map[string]informer.Informer)
+	p.withJobs = make(map[string]informer.JobsStat)
+	return nil
+}
+
+// Workers provides BaseProcess slice with workers for the requested plugin
+func (p *Plugin) Workers(name string) []*process.State {
+	svc, ok := p.withWorkers[name]
+	if !ok {
+		return nil
+	}
+
+	return svc.Workers()
+}
+
+// Jobs provides information about jobs for the registered plugin using jobs
+func (p *Plugin) Jobs(name string) []*jobs.State {
+	svc, ok := p.withJobs[name]
+	if !ok {
+		return nil
+	}
+
+	st, err := svc.JobsState(context.Background())
+	if err != nil {
+		// skip errors here
+		return nil
+	}
+
+	return st
+}
+
+// Collects declares services to be collected.
+func (p *Plugin) Collects() []interface{} {
+	return []interface{}{
+		p.CollectWorkers,
+		p.CollectJobs,
+	}
+}
+
+// CollectWorkers obtains plugins with workers inside.
+func (p *Plugin) CollectWorkers(name endure.Named, r informer.Informer) {
+	p.withWorkers[name.Name()] = r
+}
+
+func (p *Plugin) CollectJobs(name endure.Named, j informer.JobsStat) {
+	p.withJobs[name.Name()] = j
+}
+
+// Name of the service.
+func (p *Plugin) Name() string {
+	return PluginName
+}
+
+// RPC returns associated rpc service.
+func (p *Plugin) RPC() interface{} {
+	return &rpc{srv: p}
+}
---
+++ rpc.go
@@ -0,0 +1,60 @@
+package informer
+
+import (
+	"github.com/roadrunner-server/api/v2/plugins/jobs"
+	"github.com/roadrunner-server/sdk/v2/state/process"
+)
+
+type rpc struct {
+	srv *Plugin
+}
+
+// WorkerList contains list of workers.
+type WorkerList struct {
+	// Workers are list of workers.
+	Workers []*process.State `json:"workers"`
+}
+
+// List all plugins with workers.
+func (rpc *rpc) List(_ bool, list *[]string) error {
+	*list = make([]string, 0, len(rpc.srv.withWorkers))
+
+	// append all plugin names to the output result
+	for name := range rpc.srv.withWorkers {
+		*list = append(*list, name)
+	}
+
+	return nil
+}
+
+// Workers state of a given service.
+func (rpc *rpc) Workers(service string, list *WorkerList) error {
+	workers := rpc.srv.Workers(service)
+	if workers == nil {
+		return nil
+	}
+
+	// write actual processes
+	list.Workers = workers
+
+	return nil
+}
+
+func (rpc *rpc) Jobs(service string, out *[]*jobs.State) error {
+	*out = rpc.srv.Jobs(service)
+	return nil
+}
+
+// sort.Sort
+
+func (w *WorkerList) Len() int {
+	return len(w.Workers)
+}
+
+func (w *WorkerList) Less(i, j int) bool {
+	return w.Workers[i].Pid < w.Workers[j].Pid
+}
+
+func (w *WorkerList) Swap(i, j int) {
+	w.Workers[i], w.Workers[j] = w.Workers[j], w.Workers[i]
+}
